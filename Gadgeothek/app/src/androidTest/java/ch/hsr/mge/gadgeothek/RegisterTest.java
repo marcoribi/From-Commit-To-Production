6ff9e0edc76f9fe3f1a8776a41588e2f328bbcaf
Marco Ribi <marco.ribi@gmail.com>
2017-10-28 13:31:07 +0200

    add regsiter end2end test

--- Gadgeothek/app/src/androidTest/java/ch/hsr/mge/gadgeothek/RegisterTest.java
+++ Gadgeothek/app/src/androidTest/java/ch/hsr/mge/gadgeothek/RegisterTest.java
@@ -10,10 +10,15 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static android.support.test.espresso.Espresso.closeSoftKeyboard;
 import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.typeText;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static ch.hsr.mge.gadgeothek.http.Endpoint.post;
+import static java.lang.String.valueOf;
 
 @RunWith(AndroidJUnit4.class)
 @LargeTest
@@ -34,5 +39,24 @@ public class RegisterTest {
     public void showsRegisterActivity() throws InterruptedException {
         // This sleep is needed because SplashActivity uses postDelayed with a delay of 2000 ms
         onView(withId(R.id.registerButton)).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void registerSuccessfully() throws InterruptedException {
+        httpProxy.mockCall(post("/public/register"), "true");
+        String loginToken = "{\"customerId\": \"1111\", \"securityToken\": \"myToken\" }";
+        httpProxy.mockCall(post("/public/login"), loginToken);
+
+        onView(withId(R.id.name)).perform(typeText("Marco Ribi"));
+        closeSoftKeyboard();
+        onView(withId(R.id.email)).perform(typeText("marco.ribi@hsr.ch"));
+        closeSoftKeyboard();
+        onView(withId(R.id.matrikelnr)).perform(typeText("1234568393"));
+        closeSoftKeyboard();
+        onView(withId(R.id.password)).perform(typeText("abcde"));
+        closeSoftKeyboard();
+        onView(withId(R.id.registerButton)).perform(click());
+
+        onView(withId(R.id.viewpager)).check(matches(isDisplayed()));
     }
 }
